0cf9518f0950a9107493fd4af6f9932460fef0ec
Tharun-hub <112345836+Tharun-hub@users.noreply.github.com>
2026-09-19 08:52:21 +0530

    Effect_Hook

---
+++ React/Effect_Hook/src/components/Scoreboard/index.jsx
@@ -0,0 +1,44 @@
+import {useState, useEffect} from 'react'
+
+import './index.css'
+
+const Scoreboard = () => {
+  const [score, setScore] = useState(0)
+
+  // useEffect hook using arrow function
+  useEffect(()=>{
+    document.title = `Score: ${score}`;
+  })
+
+  // useEffect hook using function declaration
+
+  /* useEffect(changeTitle);
+  function changeTitle()
+  {
+    document.title = `Score: ${score}`;
+  } */
+
+  const onIncrement = () => {
+    setScore(prevScore => prevScore + 1)
+  }
+
+  const onDecrement = () => {
+    setScore(prevScore => prevScore - 1)
+  }
+
+  return (
+    <div className="main-container">
+      <p className="score-text">{score}</p>
+      <div className="buttons-container">
+        <button className="button" onClick={onIncrement} type="button">
+          Increase
+        </button>
+        <button className="button" onClick={onDecrement} type="button">
+          Decrease
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Scoreboard
