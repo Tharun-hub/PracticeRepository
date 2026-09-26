cfcaebb1cb08e85c32ebb6fdc8671a73513db7c9
Tharun-hub <112345836+Tharun-hub@users.noreply.github.com>
2026-09-26 08:41:17 +0530

    Removed BrowserRouter from app.jsx

--- React/React_Routing/src/App.jsx
+++ React/React_Routing/src/App.jsx
@@ -1,13 +1,12 @@
 /* import {Fragment} from 'react' */
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { Route, Routes } from 'react-router'
 import Header from './components/Header'
 import About from './components/About'
 import Contact from './components/Contact'
 import Home from './components/Home'
 import NotFound from './components/NotFound'
 
-const App = () => (
-  <BrowserRouter>
+{/* <BrowserRouter>
     <Header />
     <Routes>
       <Route path='/' element={<Home />}/>
@@ -15,7 +14,20 @@ const App = () => (
       <Route path='/contact' element={<Contact />}/>
       <Route path='*' element={<NotFound />}/>
     </Routes>
-  </BrowserRouter>
+  </BrowserRouter> */}
+
+  
+const App = () => (
+  
+  <>
+  <Header />
+  <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='/contact' element={<Contact />}/>
+      <Route path='*' element={<NotFound />}/>
+    </Routes>
+  </>
 )
 
 export default App
